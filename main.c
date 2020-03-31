3fb9a5090dafb99b06c221dcb30fd54dc8041379
Anastasiya443 <tominaanastasiya2001@gmail.com>
2020-03-31 15:19:39 +0700

    Calculation of perimeter and area for a circle

--- main.c
+++ main.c
@@ -1,33 +1,63 @@
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    printf("Enter a figure. For instance, circle(2 3,6)");
-    char str[80];
-    char circle[] = "circle";
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-    int f = 0;
-    int i, x, y, r, k = 0, j;
-    printf("\n");
-    fgets(str, 80, stdin);
-    printf("\n");
-    printf("\n");
+int main() {
+char str[80];
+char circle[] = "circle";
+char strx[80];
+char stry[80];
+char strr[80];
+int f=0;
+int p=0;
+char * n;
+int i,x,y,r=0,k=0,j=0,l=0,u=0,fig=1;
+printf("Enter a figure. For instance, circle(2 3,6). After entering the figures write the 7\n");
+while (1) {
+fgets(str, 80, stdin);
+if (str[0] == '7') break;
+if (strncmp(str, circle, 6)) {
+printf("Incorrect input\n");
+continue;
+}
+if (!(strncmp( str, circle, 6))) {
+printf("%d figure:",fig);
+fig++;
+printf("circle\n");
+i = 0;
+while (!('0' <= str[i] && str[i] <= '9' || str[i]=='-')) {
+j = i;//j+1 = начало x
+i++;
+}
+for (i = j + 1; str[i] != '\0'; i++)
+strx[i - j - 1] = str[i];
+x= atof(strx);
+for (i = 0; str[i] != ','; i++)
+k = i;// str[k+1] = ','
+i = k + 1;
+while (!('0' <= str[i] && str[i] <= '9' || str[i] == '-')) {
+l = i;//l+1 = начало r
+i++;
+}
+for (i = l + 1; str[i] != '\0'; i++)
+strr[i - l - 1] = str[i];
+r = atof(strr);
+i = k + 1;
+while (!('0' <= str[i] && str[i] <= '9')) {
+p = i;//p-1 = конец последней цифры у
+i--;
+}
+for (i = p - 1; str[i] != ' '; i--)
+u = i;// u первая цифра у
+for (i = u; str[i] != '\0'; i++)
+stry[i - u] = str[i];
+y = atof(stry);
+printf("Coord: %d %d\n", x,y);
+printf("Radius: %d\n",r);
+printf("Area: %.4f\n", 3.14*r*r);
+printf("Circumference: %.4f\n\n", 2*3.14 * r);
+}
 
-    if (!(strncmp(str, circle, 6))) {
-        f = 1;
-        x = str[7];
-        y = str[9];
-        r = str[11];
-    }
-    if (f == 0)
-        printf("Incorrect input\n");
-
-    if (f == 1) {
-        printf("figure:");
-        printf("circle\n");
-        printf("Coord:%4c %4c\n", x, y);
-        printf("Radius:%4c\n", r);
-    }
-
-    return 0;
+}
+return 0;
 }
